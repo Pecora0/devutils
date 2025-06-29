4a4c73f3f93a73034ae57575354beefa1fe0e718
pecora <pecora@posteo.de>
2025-06-29 11:00:34 +0200

    added UNUSED, UNREACHABLE, UNIMPLEMENTED, CHECK_PRINTF

---
+++ devutils.h
@@ -0,0 +1,19 @@
+#ifndef DEVUTILS_H
+#define DEVUTILS_H
+
+#include <stdlib.h>
+
+// Copied from https://github.com/tsoding/nob.h/blob/main/nob.h
+#define UNUSED(value) (void)(value)
+#define UNREACHABLE(message) do { fprintf(stderr, "%s:%d: UNREACHABLE: %s\n", __FILE__, __LINE__, message); abort(); } while(0)
+
+#define UNIMPLEMENTED(message) do { fprintf(stderr, "%s:%d: UNIMPLEMENTED: %s\n", __FILE__, __LINE__, message); abort(); } while(0)
+
+// Copied from https://github.com/tsoding/la/blob/master/lag.c
+#if defined(__GNUC__) || defined(__clang__)
+#define CHECK_PRINTF_FMT(a, b) __attribute__ ((format (printf, a, b)))
+#else
+#define CHECK_PRINTF_FMT(...)
+#endif
+
+#endif // DEVUTILS_H
